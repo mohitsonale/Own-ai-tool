8117617542d95cec17e145acf110e615755577d6
Mohit Sonale <mohitsonale@gmail.com>
2025-10-03 23:39:41 +0530

    ai

---
+++ src/component/QuestionAnswer.jsx
@@ -0,0 +1,26 @@
+import Answer from "./Answers";
+
+function QuestionAnswer({item,index}){
+
+    return(
+        <>
+
+         <div key={index + Math.random()} className={item.type == 'q' ? 'flex justify-end' : ''}>
+                    {
+                      item.type == 'q' ?
+                        <li key={index + Math.random()} className="text-right p-1 border-8 dark:bg-zinc-700 dark:border-zinc-700 bg-red-100 border-red-100 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit"> <Answer ans={item.text} index={index} totalresult={1} type={item.type} /></li> :
+                        item.text.map((ansItem, ansIndex) => (
+                          <li key={ansIndex + Math.random()} className="text-left p-1"><Answer ans={ansItem} index={index} totalresult={1} type={item.type} /></li>
+                        ))
+
+
+
+                    }
+
+                  </div>
+        
+        </>
+    )
+}
+
+export default QuestionAnswer;
